ba92b33e9b54a79e0713a72cbb43ba23b1e8a1ac
deepak reddy <kdeepakreddy610@gmail.com>
2026-07-18 12:11:34 +0530

    added toggle button

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,30 @@
- alert("DAENERYS is Ineviteble")
+// //  alert("DAENERYS is Ineviteble")
 // console.log("Hello");
+//  Data= {
+// "name" : "John",
+// "Gender" : "Male"
+
+// }
+var user= [
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "image" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "image" : "jane.png"
+    }
+]
+var id=0;
+
+function toggleuser(){
+    id=(id+1)%2;
+    var username=document.getElementById("user-name");
+    username.innerHTML=user[id].name;
+    var usergender=document.getElementById("user-gender");
+    usergender.innerHTML=user[id].gender;
+    var userimage=document.getElementById("user-image");
+    userimage.src=user[id].image;
+}
