009aa23144c44c8e3d6e2325780e32040f25f732
deepak reddy <kdeepakreddy610@gmail.com>
2026-07-18 13:02:50 +0530

    ADDED RANDOM USER

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -28,3 +28,27 @@ function toggleuser(){
     var userimage=document.getElementById("user-image");
     userimage.src=user[id].image;
 }
+function randomuser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data)
+    {
+        var userData =  data.results[0];
+
+         // 1: image
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            // 2: name
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " +
+                                    userData.name.last;
+
+            // 3: gender
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        
+}
